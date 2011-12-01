7bf89e70646f887cf88b01e7b09e96b91b444966
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-12-01 22:56:20 +0530

    Updated spec_helper to use RSpec-2 api

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__),'..','lib'))
 
 require 'game_of_life'
-require 'spec'
+#require 'spec'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   # Nothing as yet...
 end
