d7d51090dd0668295bfb55de2b4e1f4e949cf9cb
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-12-01 21:04:52 +0530

    Initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+#File content source: https://github.com/coreyhaines/practice_game_of_life/blob/master/spec/spec_helper.rb
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__),'..','lib'))
+
+require 'game_of_life'
+require 'spec'
+
+Spec::Runner.configure do |config|
+  # Nothing as yet...
+end
